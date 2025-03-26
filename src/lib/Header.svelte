593e68d914a3b28aed78f0df0bbb89b0b220e5f7
AlecPts <alecpetitsiejak@gmail.com>
2025-03-27 00:05:55 +0100

    add first basics to the site

---
+++ src/lib/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+import {DarkMode} from "flowbite-svelte";
+</script>
+
+<header class="w-full fixed p-4 bg-background dark:bg-background-dark z-10">
+    <div class="max-w-screen-lg mx-auto sm:max-w-full md:max-w-screen-lg lg:max-w-screen-lg xl:max-w-screen-lg flex justify-between items-center">
+        <a href="#"><h1 class="text-xl">Alec</h1></a>
+        <DarkMode />
+    </div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,74 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import profileImage from "$lib/assets/profile_image.png";
+
+    // Mange SVG lines
+    const nbLinesSvg = 5;
+    const spacing = -5;
+    const tabLinesOffset = [];
+    for (let i = 0; i < nbLinesSvg; i++) {
+        tabLinesOffset.push(i * spacing);
+    }
+
+</script>
+
+<div>
+<!--    <svg class="line absolute" viewBox="0 0 600 800" width="100%" height="100%">-->
+<!--        {#each tabLinesOffset as offset}-->
+<!--            <path-->
+<!--                    d="-->
+<!--                    M {580 + offset},0-->
+<!--                    V 170-->
+<!--                    L {530 + offset} 220-->
+<!--                    H {300 + offset}-->
+<!--                    V {800 + offset}-->
+<!--                    "-->
+
+<!--                    fill="none" stroke="var(&#45;&#45;color-line)" stroke-width="1.5px"-->
+<!--            />-->
+<!--        {/each}-->
+<!--    </svg>-->
+    <section id="home-section" class="scroll-section">
+        <div class="h-40"></div>
+
+        <div class="text-center m-10">
+            <h1 class="text-6xl mb-2">Alec Petit-Siejak</h1>
+            <h2 class="text-xl">Étudiant en informatique</h2>
+        </div>
+
+        <div class="h-20"></div>
+    </section>
+
+<!--    <div class="flex justify-center">-->
+<!--        <div class="bg-primary w-3xs rounded-xl">-->
+<!--            &lt;!&ndash;            <img src={profileImage} alt="Profile Image" />&ndash;&gt;-->
+<!--        </div>-->
+<!--    </div>-->
+
+    <!--About Me-->
+    <div class="text-2xl my-10">
+        <h1>À propos de moi</h1>
+    </div>
+
+    <!--Projects-->
+    <div class="text-2xl my-10">
+        <h1>Mes projets</h1>
+    </div>
+
+    <!--Skills-->
+    <div class="text-2xl my-10">
+        <h1>Mes compétences</h1>
+    </div>
+
+    <!--Contact Me-->
+    <div class="text-2xl my-10">
+        <h1>Contactez-moi</h1>
+    </div>
+
+    <div class="h-96"></div>
+    <div class="h-96"></div>
+    <div class="h-96"></div>
+
+</div>
+
+
+
